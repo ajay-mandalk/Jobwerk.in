0286e25fb52970f2fb3bbe3384d3e4923eb492cf
Ajay <ajaykumar84842@gamil.com>
2024-11-02 23:22:25 +0530

    some bug removed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
-
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://fkmquaeeduekpvhfpkdd.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY!
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
