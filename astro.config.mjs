2aad4f9d1724dfede2314a944fc1a50350b542be
Ajay <ajaykumar84842@gamil.com>
2025-03-02 18:06:27 +0530

    Add sitemap integration, remove unused components, and update navbar and index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import clerk from "@clerk/astro";
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [react(), tailwind(), clerk()],
+  integrations: [react(), tailwind(), clerk(), sitemap()],
   site: "https://www.jobwerk.in",
   output: 'server',
 
